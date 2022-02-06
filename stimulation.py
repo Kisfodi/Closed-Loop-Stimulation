0c745e75f15d9930b50fa8e4f53c36f71d4d295d
Kisfodi <76233162+Kisfodi@users.noreply.github.com>
2022-02-06 21:52:08 +0100

    Add files via upload

--- stimulation.py
+++ stimulation.py
@@ -1,7 +1,29 @@
 import json
-
+import pickle
+import os
+from retinotopic_mapping import DisplayLogAnalysis
 
 class Stimulation:
-    def __init__(self, filename):
-        with open(filename, 'r') as infile:
-            self.json_keys = json.load(infile)
+    def __init__(self, pathname):
+        self.pathname = pathname
+        self.json_path = self.pathname.replace('.pkl', '.json')
+        self.filename,self.extension = os.path.splitext(self.pathname)
+        if ("pkl" in self.extension):
+            self.log = DisplayLogAnalysis.DisplayLogAnalyzer(self.pathname)
+            self.log.stim_block_extractor()
+            self.log.save_to_json()
+            self.load_json()
+        elif ("json" in self.extension):
+             self.load_json()
+
+        # self.stim_type = self.log['stim_parameters']['stim_type']
+        # if ('CombinedStimuli' in self.stim_type):
+        #     self.indices = self.log['iteration_indices']
+        # elif ("DriftingGratingCircle" in self.stim_type):
+        #     self.indices = self.log['direction_indices']
+        # else:
+        #     self.indices = []
+        #     print("Not implemented yet!!!")
+    def load_json(self):
+        with open (self.json_path) as json_file:
+            self.log = json.load(json_file)
--- visualization.py
+++ visualization.py
@@ -1,8 +1,11 @@
+import queue
+
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import matplotlib
 import stimulation
+import csv
 
 matplotlib.use("TkAgg")
 
@@ -12,7 +15,7 @@ class Visualizer:
     A class used for visualization of real-time data
     """
 
-    def __init__(self, f, window_size=50, interval_size=1, counter=1):
+    def __init__(self, f, fx, window_size=50, interval_size=1, counter=1):
         """
         :param f: The container (mainly a Queue) which contains real-time data
         :param window_size: The size of the data we want to visualize
@@ -35,53 +38,60 @@ class Visualizer:
         self.window_size = window_size
         self.interval_size = interval_size
         self.x = []
+        self.xval = []
         self.stim_index = []
         self.non_stim_index = []
         self.y = []
         self.ysq = []
         self.m = []
         self.f = f
+        self.fx = fx
         self.counter = counter
         self.stimulus = stimulation.Stimulation('keys.json')
         self.is_stim = False
         self.stim = []
         self.non_stim = []
+        self.block_means = []
 
     def update(self, i):
-        # TODO
+        self.x.append(i)
+        #TODO
         # Solve IndexError: list index out of range error
         # Cause: not enough keys
         # Solution terminating signal / Exception handlding
 
         # print(self.f.qsize())
-        self.x.append(i)
-        print(self.y[i:])
-        print(i)
+        next_y = self.f.get()
+        print("Timestamp empty status: {status}".format(status = self.fx.empty()))
+        if (not self.fx.empty()):
+            next_x = self.fx.get()
+            self.xval.append(next_x)
+        print("x value: {xx} - y value: {yy}".format(xx=self.xval[-1:],yy=self.y[-1:]))
         try:
-            print(self.stimulus.json_keys[i])
+            print(self.stimulus.log[i])
         except IndexError:
             print("No more keys...")
         print("Is stimulus {index}".format(index=self.is_stim))
-        self.y.append(self.f.get())
-        self.ysq.append(-1 * (self.f.get()))
+
+        self.y.append(next_y)
+
+        with open("C:\Egyetem\TTK\Closed loop stimulation_project\Test_xy.csv",'w',newline='') as ofile:
+            result_writer = csv.writer(ofile, delimiter = ',')
+            for ind in range(len(self.y)):
+                result_writer.writerow([self.xval[ind],self.y[ind]])
+        self.ysq.append(-1 * (next_y))
         self.m.append(np.mean(self.y))
+
+        #TODO
+        # make stimulus selection work for iteration/direction indices/timestamps
         try:
-            if self.stimulus.json_keys[i] and self.y[i:] != []:
+            if self.stimulus.log[i] and self.y[i:] != []:
                 self.stim.append(self.y[i])
                 self.stim_index.append(i)
 
-            # self.is_stim = True
-            # ha 1es kulcsot olvas be és is_stim=false
-            # stimulus konténer (új lista)
-            # stimulus.append
-            # is_stim = true
-
-            if not self.stimulus.json_keys[i] and self.y[i:] != []:
+            if not self.stimulus.log[i] and self.y[i:] != []:
                 self.non_stim.append(self.y[i])
                 self.non_stim_index.append(i)
-        # ha 1es kulcsot olvas be és is_stim=true
-        # stimulus konténer (új lista)
-        # stimulus.append
         except IndexError:
             pass
         # gs = ff.add_gridspec(2, 2)
@@ -94,16 +104,13 @@ class Visualizer:
                     if ax != self.axs[1, 1]:
                         ax.cla()
             self.axbig.cla()
-            # self.ax.cla()
-            # self.ax2.cla()
-            # self.ax3.cla()
-            # self.ax4.cla()
-            # self.ax5.cla()
             self.axbig.plot(self.x[-self.window_size:], self.y[-self.window_size:])
-            # akkor plotoljon, ha az egyesek után 0 lesz
-            # if flag:
+
+            #TODO
+            # Types of data to put into output
+            # What kind of data (DF/F means, simple activity)
             try:
-                if not self.stimulus.json_keys[i] and self.is_stim:
+                if not self.stimulus.log[i] and self.is_stim:
                     print("Starting non-stimulus block at index {index}...".format(index=i))
                     print("Plotting terminated stimulus block...")
 
@@ -111,11 +118,18 @@ class Visualizer:
                     self.axs[1, 1].plot(self.stim_index, self.stim, color='green')
                     self.axs[1, 1].scatter(self.stim_index[-1], self.stim[-1], color='green')
                     self.axs[1, 1].text(np.mean(np.array(self.stim_index)), np.mean(np.array(self.stim)), "Stimulus")
+
+                    self.block_means.append([np.mean(np.array(self.stim)),"Stim"])
+                    with open("C:\Egyetem\TTK\Closed loop stimulation_project\Test_block_means.csv", 'w', newline='') as ofile:
+                        result_writer = csv.writer(ofile, delimiter=',')
+                        for ind in range(len(self.block_means)):
+                            result_writer.writerow([ind,self.block_means[ind][0],self.block_means[ind][1]])
+
                     self.stim.clear()
                     self.stim_index.clear()
                     self.is_stim = False
 
-                if self.stimulus.json_keys[i] and not self.is_stim:
+                if self.stimulus.log[i] and not self.is_stim:
                     print("Starting stimulus block at index {index}...".format(index=i))
                     print("Plotting terminated non-stimulus block...")
 
@@ -124,6 +138,15 @@ class Visualizer:
                     self.axs[1, 1].scatter(self.non_stim_index[-1], self.non_stim[-1], color='red')
                     self.axs[1, 1].text(np.mean(np.array(self.non_stim_index)), np.mean(np.array(self.non_stim)),
                                         "Not stimulus")
+
+                    self.block_means.append([np.mean(np.array(self.non_stim)),"Non_Stim"])
+                    with open("C:\Egyetem\TTK\Closed loop stimulation_project\Test_block_means.csv", 'w',
+                          newline='') as ofile:
+                        result_writer = csv.writer(ofile, delimiter=',')
+                        for ind in range(len(self.block_means)):
+                            result_writer.writerow([ind, self.block_means[ind][0],self.block_means[ind][1]])
+
+
                     self.non_stim.clear()
                     self.non_stim_index.clear()
                     self.is_stim = True
@@ -134,17 +157,16 @@ class Visualizer:
             # plot stimulus
             # stimulus kiürítése
             # is_stim = false
-
-            self.axs[1, 0].plot(self.x[-self.window_size:], 2 * np.abs(self.y[-self.window_size:]))
+            self.axs[1, 0].plot(self.x[-self.window_size:], (self.xval[-self.window_size:]))
             # self.axs[1, 1].plot(self.x[-self.window_size:], self.ysq[-self.window_size:])
             self.axs[2, 0].plot(self.x[-self.window_size:], self.m[-self.window_size:])
-            self.axs[2, 1].plot(self.x[-self.window_size:], self.ysq[-self.window_size:])
+            self.axs[2, 1].scatter(self.xval[-self.window_size:], self.y[-self.window_size:])
             self.axbig.scatter(i, self.y[-1])
             self.axbig.text(i - 1, self.y[-1] + 2, "{}".format(self.y[-1]))
-            self.axs[1, 0].scatter(i, 2 * np.abs(self.y[-1]))
+            self.axs[1, 0].scatter(i, (self.xval[-1]))
             # self.axs[1, 1].scatter(i, self.ysq[-1])
             self.axs[2, 0].scatter(i, self.m[-1])
-            self.axs[2, 1].scatter(i, self.ysq[-1])
+           # self.axs[2, 1].scatter(i, self.y[-1])
 
     def animate(self):
         ani = animation.FuncAnimation(fig=self.fig, func=self.update, interval=self.interval_size)
